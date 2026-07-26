cbffaeb396fa0a057af80be8222b41aac05fe126
Rayan <rayanjunaid25@gmail.com>
2026-07-26 17:50:44 +0100

    good enough to submit i think?

--- arena.gd
+++ arena.gd
@@ -2,7 +2,9 @@ extends Node
 
 @export var enemy_scene: PackedScene
 var hp := 10
-const initial_time := 5.0 
+var score := 0
+@onready var health_bar = $CanvasLayer/HealthBar
+const initial_time := 5.0
 var enemies: Dictionary[Vector2i, Enemy] = {}
 var last_spawned_enemies: Dictionary[Vector2i, Enemy] = {}
 var direction: Vector2i
@@ -13,6 +15,9 @@ var facing_up := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$SpawnTimer.wait_time = initial_time
+	$CanvasLayer/ScoreLabel.text = "Score: 0"
+	health_bar.max_value = hp
+	health_bar.value = hp
 	for sign in [-1, 1]:
 		for axis in [0, 1]:
 			var dir = Vector2i(0, 0)
@@ -22,7 +27,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$SpawnTimer.wait_time = initial_time * exp(-0.0001 * Time.get_ticks_msec()) # use diff function
+	$SpawnTimer.wait_time = initial_time / (-0.00002 * Time.get_ticks_msec() + 1)
 	$HitboxSprite.visible = true
 	# Move player to a marker based on input, and set player direction
 	if Input.is_action_pressed("left"):
@@ -37,7 +42,7 @@ func _process(delta: float) -> void:
 		direction = Vector2i(0, 0)
 		$HitboxSprite.visible = false
 	$HitboxSprite.position = direction * Globals.hitbox_offset
-	$PlayerSprite.position = direction * (Globals.hitbox_offset - 100)
+	$PlayerSprite.position = direction * (Globals.hitbox_offset - 50)
 	
 	# idle animation handling
 	if direction == Vector2i(0, -1) and $PlayerSprite.animation.begins_with("idle"):
@@ -58,7 +63,9 @@ func _process(delta: float) -> void:
 		can_move = false
 		play_player_animation(enemy.move)
 		$Label.text = Globals.Move.keys()[enemy.move]
-		enemy.queue_free()
+		enemy.die()
+		score += 1
+		$CanvasLayer/ScoreLabel.text = "Score: " + str(score)
 
 
 func _on_spawn_timer_timeout() -> void:
@@ -66,26 +73,49 @@ func _on_spawn_timer_timeout() -> void:
 	var enemy_direction = Vector2i(0, 0)
 	var enemy_move = randi_range(0, 2)
 	enemy_direction[randi_range(0, 1)] = randi_range(0, 1) * 2 - 1
-	var enemy_countdown = 2.0
+	var enemy_countdown = 5.0
 	var last_enemy = last_spawned_enemies[enemy_direction]
 	if last_enemy != null:
 		enemy_countdown = last_enemy.get_node("DespawnTimer").time_left
 	
-	enemy.construct(enemy_direction, enemy_move, randf_range(enemy_countdown, enemy_countdown + 1)) # stop it randomly going too fast by altering lb
+	enemy.construct(enemy_direction, enemy_move, enemy_countdown) # stop it randomly going too fast by altering lb
 	enemy.attacked.connect(_on_enemy_attacked)
 	add_child(enemy)
+	enemy.play_enemy_animation("run")
 	last_spawned_enemies[enemy_direction] = enemy
 
 
 func _on_enemy_attacked(enemy: Enemy) -> void:
 	enemies[enemy.direction] = enemy
 	can_move = true
+	enemy.play_enemy_animation("attack")
 	await enemy.despawn
 	if can_move: 
 		$Label.text = "Too early/late"
 		can_move = false
+		damage(1)
 	
 	enemies[enemy.direction] = null
+
+
+func damage(amount: int):
+	hp -= amount
+	hp = max(hp, 0)
+	create_tween().tween_property(
+		health_bar,
+		"value",
+		hp,
+		0.15
+		)
+
+	if hp == 0:
+		game_over()
+
+
+func game_over():
+	get_tree().paused = true
+	$CanvasLayer/GameOverScreen/FinalScoreLabel.text = "Final Score: " + str(score)
+	$CanvasLayer/GameOverScreen.visible = true
 
 func play_player_animation(move: Globals.Move):
 	var move_str := ""
@@ -121,3 +151,9 @@ func _on_player_sprite_animation_finished() -> void:
 		$PlayerSprite.play("idle_up")
 	else:
 		$PlayerSprite.play("idle_down")
+
+
+func _on_retry_button_pressed() -> void:
+	print("Retry pressed")
+	get_tree().paused = false
+	get_tree().reload_current_scene()
--- enemy.gd
+++ enemy.gd
@@ -4,14 +4,41 @@ extends Node2D
 var direction: Vector2i
 var move: Globals.Move
 var damage: int
+var dead := false
 var _origin
 signal attacked(enemy: Enemy)
 signal despawn()
 
+const ORC_FRAMES = [
+	preload("res://Assets/Orc1Frames.tres"),
+	preload("res://Assets/Orc2Frames.tres"),
+	preload("res://Assets/Orc3Frames.tres"),
+]
+
+const PLANT_FRAMES = [
+	preload("res://Assets/Plant1Frames.tres"),
+	preload("res://Assets/Plant2Frames.tres"),
+	preload("res://Assets/Plant3Frames.tres"),
+]
+
+const SLIME_FRAMES = [
+	preload("res://Assets/Slime1Frames.tres"),
+	preload("res://Assets/Slime2Frames.tres"),
+	preload("res://Assets/Slime3Frames.tres"),
+]
 
 func construct(direction: Vector2i, move: Globals.Move, countdown: float):
 	self.direction = direction
 	self.move = move
+	
+	match move:
+		Globals.Move.PARRY:
+			$EnemySprite.sprite_frames = ORC_FRAMES.pick_random()
+		Globals.Move.SLASH:
+			$EnemySprite.sprite_frames = PLANT_FRAMES.pick_random()
+		Globals.Move.THRUST:
+			$EnemySprite.sprite_frames = SLIME_FRAMES.pick_random()
+	
 	$DespawnTimer.wait_time = countdown
 	$AttackTimer.wait_time = countdown - Globals.move_window
 	$EnemyAttackLabel.text = Globals.Move.keys()[move]
@@ -36,10 +63,46 @@ func time_to_progress(time: float) -> float:
 	else: 
 		return 1
 
+
+func die():
+	if dead:
+		return
+
+	dead = true
+
+	$DespawnTimer.stop()
+	$AttackTimer.stop()
+
+	play_enemy_animation("die")
+
+	await $EnemySprite.animation_finished
+	queue_free()
+
+
 func _on_attack_timer_timeout() -> void:
 	attacked.emit(self)
 
 
 func _on_travel_timer_timeout() -> void:
 	despawn.emit()
-	queue_free()
+	await die()
+
+func play_enemy_animation(action: String):
+	var anim = action + "_" + direction_string()
+	$EnemySprite.play(anim)
+	$EnemySprite.set_frame_and_progress(0, 0.0)
+	
+	if action == "attack" or action == "die":
+		$EnemySprite.speed_scale = 1/Globals.move_window
+
+func direction_string() -> String:
+	match direction:
+		Vector2i(-1, 0):
+			return "left"
+		Vector2i(1, 0):
+			return "right"
+		Vector2i(0, -1):
+			return "up"
+		Vector2i(0, 1):
+			return "down"
+	return "down"
